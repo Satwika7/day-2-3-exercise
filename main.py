8720af7bbfa05eab4ac71360cacfb88b0837f381
Satwika7 <113924355+Satwika7@users.noreply.github.com>
2022-09-20 08:55:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+remain_age = 90-int(age)
+months_left = 12*remain_age
+weeks_left = 52*remain_age
+days_left = remain_age*365
+print(f"you have {days_left} days, {weeks_left} ,and {months_left} left.")
+
+
+
+
+
+
+
+
